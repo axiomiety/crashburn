052e0c06296b0b5d774d4a1109c3cb63cb0bc826
User Axiomiety <axiomiety@gmail.com>
2012-09-03 23:09:05 +0000

    init

---
+++ setup_git.sh
@@ -0,0 +1,4 @@
+git config --global user.email 'axiomiety@gmail.com'
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+git remote add origin https://github.com/aximiety/otw.git
