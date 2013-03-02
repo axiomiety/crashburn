b9b9cfeccc017b5765977bd72a1bc5841f9c6ebb
axiomiety <axiomiety@gmail.com>
2013-03-02 16:35:24 +0000

    + .gitignore so we don't check in tmp files (mainly from vim)

--- setup_git.sh
+++ setup_git.sh
@@ -1,6 +1,7 @@
 git config --global user.email 'axiomiety@gmail.com'
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
+git config --global core.excludesfile ~/.gitignore
 cd ~
 git clone https://github.com/axiomiety/setup.git
 git clone https://github.com/axiomiety/git.git
@@ -8,3 +9,5 @@ cd setup
 git remote add setup https://github.com/axiomiety/setup.git
 cd ../otw
 git remote add otw https://github.com/axiomiety/otw.git
+
+git config --global core.editor "vim"
