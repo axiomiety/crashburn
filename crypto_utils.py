fc08b3313dd052993ab00aa11ef87b002f8295c7
axiomiety <axiomiety@gmail.com>
2013-06-11 21:27:37 +0100

    small update

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,18 +1,51 @@
 '''
 Originally written to help me complete the Matasano Crypto challenge
 This collection of utils is quite aggressive - most methods use asserts for sanity checks. They also pretty much operate exclusively on bytes.
+Where possible, imports are done at the fn level so you don't need to install modules for stuff you won't use.
+
+PREREQUISITES:
+  python3-crypto
 '''
+import  binascii
 import  random
 import  unittest
-#from  Crypto.Cipher import  AES
+from    collections import  Counter
+
+def is_using_ECB(raw, blocksize):
+  indexes = range(0,len(raw), blocksize)
+  d = []
+  for (start,end) in zip(indexes, indexes[1:]):
+    d.append( raw[start:end] )
+  cn = Counter(d)
+  if cn.most_common()[0][1] > 1:
+    return True
+  else:
+    return False
+
+def generate_random_key(length=3):
+  bar = bytearray()
+  for i in range(length):
+    bar.append( random.randint(0,255) )
+  return bytes(bar)
+
+def generate_random_AES_key():
+  return generate_random_key(length=16) # well, can be 16, 24, 32
+
+def b64_file_to_bytes(filename):
+
+  with open(filename, 'rU') as f:
+    lines = f.readlines()
+
+  data = ''.join([l.strip() for l in lines])
+  return bytes(binascii.a2b_base64(data.encode('ascii')))
 
 def pad(b, length):
   ''' a PKCS#7 padding implementation '''
   assert(isinstance(b, bytes))
   assert(len(b) <= length)
-  assert(length < 256) # as we're padding with bytes
 
   pad_length = length - len(b)
+  assert(pad_length < 256) # as we're padding with bytes
   if pad_length:
     padding = bytes((pad_length,))*pad_length
     return b + padding
@@ -44,22 +77,87 @@ def slice(b, idx, jump):
     idx += jump
   return bytes(bar)
 
-def xor_encrypt(k, txt):
-  kk = fill_key(k, txt)
+def get_blocks(data, size=16):
+  assert( isinstance(data, bytes) )
+
+  # make data a multiple of size bits
+  if len(data)%size != 0:
+    padded_len = size*(len(data)//size + 1)
+    data = pad(data, padded_len)
+
+  # divide data in size chunks
+  num_blocks = len(data)//size
+
+  blocks = []
+  for i in range(0, num_blocks):
+    st = i*size
+    blocks.append(data[st:st+size])
+
+  return blocks
+
+def xor_encrypt(k, txt, pad=True):
+  assert( isinstance(k, bytes) )
+  assert( isinstance(txt, bytes) )
+  kk = fill_key(k, txt) if pad else k
   return bytes([a^b for (a,b) in zip(kk, txt)])
 
-class XOREncryptionTest(unittest.TestCase):
-  
-  @staticmethod
-  def _generate_random_key(length=3):
-    bar = bytearray()
-    for i in range(length):
-      bar.append( random.randint(0,255) )
-    return bytes(bar)
+def aes_encrypt_cbc(k, txt, iv):
+  assert( isinstance(k, bytes) )
+  assert( isinstance(txt, bytes) )
+  assert( isinstance(iv, bytes) )
+  assert( len(iv) == 16 )
+
+  # for each chunk, xor with previous one
+  #   -> encrypt
+  from Crypto.Cipher import AES
+  mode = AES.MODE_ECB
+  #TODO: check k is either 16, 24 or 32 bytes long
+  cr = AES.new(k, mode)
+
+  prev_block = iv
+  blocks = get_blocks(txt, size=16)
+  enc = bytearray()
+  for block in blocks:
+    prev_block = cr.encrypt(xor_encrypt(prev_block, block, pad=False))
+    enc.extend(prev_block)
+
+  return bytes(enc)
+
+def aes_decrypt_cbc(k, txt, iv):
+  assert( isinstance(k, bytes) )
+  assert( isinstance(txt, bytes) )
+  assert( isinstance(iv, bytes) )
+  assert( len(iv) == 16 )
+
+  from Crypto.Cipher import AES
+  mode = AES.MODE_ECB
+  cr = AES.new(k, mode)
+
+  prev_block = iv
+  blocks = get_blocks(txt, size=16)
+  dec = bytearray()
+  for block in blocks:
+    dec.extend(xor_encrypt(cr.decrypt(block), prev_block, pad=False))
+    #prev_block = block
+    prev_block = block
+
+  return bytes(dec)
+
+class AESTest(unittest.TestCase):
+
+  def test_inverse(self):
+    plaintext = b'this is a text with more than 16 bytes'
+    key = b'yellow submarine'
+    iv = pad(b'1', 16)
+    cr = aes_encrypt_cbc(key, plaintext, iv)
+    dec = aes_decrypt_cbc(key, cr, iv)
+    self.assertEqual( dec[:len(plaintext)], plaintext )
+
+class XORTest(unittest.TestCase):
 
   def test_xor_encrypt(self):
     text = b'Vanilla Ice Ice Baby!'
-    randomkey = XOREncryptionTest._generate_random_key(length=random.randint(1,len(text)))
+    randomkey = generate_random_key(length=random.randint(1,len(text)))
     s = xor_encrypt(randomkey, text)
     self.assertEqual( text, xor_encrypt(randomkey, s) )
 
@@ -83,6 +181,11 @@ class HelpersTest(unittest.TestCase):
 
   def test_hamming_distance(self):
     self.assertEqual( 37, hamming_distance(b'this is a test', b'wokka wokka!!!'))
+
+  def test_get_blocks(self):
+    data = b'abcdefghi'
+    blocks = get_blocks(data, 4)
+    self.assertEqual( [b'abcd', b'efgh', b'i\x03\x03\x03'], blocks )
 
 if __name__ == "__main__":
     unittest.main()
