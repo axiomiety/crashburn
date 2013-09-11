13d513f1a0965b01687d9722925b21d435599caa
axiomiety <axiomiety@gmail.com>
2013-09-11 20:16:57 +0100

    cmd-line update

--- glacier_wrapper.py
+++ glacier_wrapper.py
@@ -1,3 +1,7 @@
+#
+# TODO: move the config file to yaml?
+# TODO: functionality to show the status of outstanding jobs
+
 import  boto
 import  logging
 import  base64
@@ -10,6 +14,7 @@ CONFIG_FILE = '/tmp/glacier.cred'
 RECORD_FILE = '/tmp/glacier.record'
 RECORD_FIELDNAMES = ['vault_name', 'archive_id', 'archive_descr', 'upload_utc_timestamp', 'size_kb', 'md5sum']
 JOB_LOG = '/tmp/glacier.job'
+JOB_FIELDNAMES = ['job_id', 'action', 'starttime_utc_timestamp' , 'status']
 
 class Glacier(object):
   def __init__(self, config_file=CONFIG_FILE, key_id=None, key=None, region_name=REGION_NAME, record_file=RECORD_FILE, job_log=JOB_LOG):
@@ -39,6 +44,13 @@ class Glacier(object):
                     },
                     fh)
 
+  def get_inventory(self, vault_name):
+    vault = self.glacier.get_vault(vault_name)
+    job_id = vault.retrieve_inventory()
+    log_job(job_id, status='init', ts=get_epoc_ts())
+
+#def log_job(
+
 def get_md5sum(f):
   '''
     We read the file in chunks of 8192 bytes, as otherwise we need to read it whole in memory
@@ -64,6 +76,7 @@ def append_record(rec, fh):
     `fh` is the filehandle for csv file keeping track of the records
   '''
   fh.write(','.join( [str(rec[col]) for col in RECORD_FIELDNAMES] ))
+  fh.write('\n') # newline is important!
 
 def parse_config_file(fh):
   '''
@@ -106,9 +119,32 @@ class ModuleMethodsTest(unittest.TestCase):
     raw = ['myvault','archiveid','descr_in_base64',1234567890,400,'file_md5sum']
     rec = {k:v for k,v in zip(RECORD_FIELDNAMES, raw)}
     append_record( rec, f )
-    self.assertEqual( f.getvalue(), ','.join(str(r) for r in raw) )
+    self.assertEqual( f.getvalue(), ','.join(str(r) for r in raw) + '\n' )
     f.close()
 
-if __name__ == "__main__":
-  from mock import patch, Mock
-  unittest.main()
+if __name__ == '__main__':
+
+  import argparse
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('-t', '--test', action='store_true')
+  parser.add_argument('-a', '--archive', dest='archive')
+  parser.add_argument('-v', '--vault', dest='vault')
+  parser.add_argument('-d', '--description', dest='descr')
+  parser.add_argument('action', nargs='?', default='upload') # positional
+  args = parser.parse_args()
+
+  if args.test:
+    import sys
+    sys.argv = [sys.argv[0]] # clean up sys.argv otherwise unittest will complain
+    #from mock import patch, Mock
+    unittest.main()
+
+  else:
+    if args.action == 'upload':
+      gl = Glacier()
+      if args.archive and args.vault:
+        descr = args.descr or ''
+        gl.upload(args.vault, args.archive, descr=descr)
+      else:
+        parser.print_help()
+    
