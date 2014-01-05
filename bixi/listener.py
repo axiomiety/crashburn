a4467703af8e0cc0aa7acb98a3412aa697048850
axiomiety <axiomiety@gmail.com>
2014-01-05 15:07:37 +0000

    bixi playground

---
+++ bixi/listener.py
@@ -0,0 +1,31 @@
+import  urllib.request
+import  json
+from    xml.etree       import ElementTree
+
+PARSING_MAP = {
+  'id'          : lambda e: e.find('id').text,
+  'name'        : lambda e: e.find('name').text,
+  'lat'         : lambda e: float(e.find('lat').text),
+  'long'        : lambda e: float(e.find('long').text),
+  'avail_bikes' : lambda e: int(e.find('nbBikes').text),
+  'empty_docks' : lambda e: int(e.find('nbEmptyDocks').text),
+  'updatets'    : lambda e: int(e.find('latestUpdateTime').text)
+  }
+
+def xml2json(root):
+  stations = []
+  for station in root.findall('station'):
+    s = dict()
+    for attrib in PARSING_MAP:
+      s[attrib] = PARSING_MAP[attrib](station)
+    stations.append(s)
+  return json.dumps({root.get('lastUpdate'): stations}, sort_keys=True)
+
+def fetch():
+  req = urllib.request.urlopen('https://toronto.bixi.com/data/bikeStations.xml')
+  raw_data = req.read()
+  if raw_data:
+    xml = ElementTree.fromstring(raw_data)
+    return xml2json(xml)
+
+#http://maps.googleapis.com/maps/api/distancematrix/json?origins=43.6633,-79.392891&destinations=43.652123,-79.28758&mode=bicycling&sensor=false
---
+++ bixi/tests.py
@@ -0,0 +1,19 @@
+import  unittest
+import  listener
+
+class ParserTest(unittest.TestCase):
+  
+  @classmethod
+  def setUpClass(cls):
+    s = '''<?xml version='1.0' encoding='UTF-8'?><stations lastUpdate="1388933822252" version="2.0"><station><id>1</id><name>Jarvis St/ Carleton St</name><terminalName>7055</terminalName><lastCommWithServer>1388933151006</lastCommWithServer><lat>43.66207</lat><long>-79.37617</long><installed>true</installed><locked>false</locked><installDate/><removalDate/><temporary>false</temporary><public>true</public><nbBikes>8</nbBikes><nbEmptyDocks>7</nbEmptyDocks><latestUpdateTime>1388899793575</latestUpdateTime></station><station><id>5</id><name>Church St/ Alexander St</name><terminalName>7044</terminalName><lastCommWithServer>1388933649708</lastCommWithServer><lat>43.6636</lat><long>-79.3806</long><installed>true</installed><locked>false</locked><installDate/><removalDate/><temporary>false</temporary><public>true</public><nbBikes>3</nbBikes><nbEmptyDocks>12</nbEmptyDocks><latestUpdateTime>1388903405786</latestUpdateTime></station></stations>'''
+    from xml.etree import ElementTree
+    cls.xml = ElementTree.fromstring(s)
+    unittest.TestCase.setUpClass()
+
+  def test_jsonEquivalent(self):
+    j = listener.xml2json(self.xml)
+    expected = '''{"1388933822252": [{"avail_bikes": 8, "empty_docks": 7, "id": "1", "lat": 43.66207, "long": -79.37617, "name": "Jarvis St/ Carleton St", "updatets": 1388899793575}, {"avail_bikes": 3, "empty_docks": 12, "id": "5", "lat": 43.6636, "long": -79.3806, "name": "Church St/ Alexander St", "updatets": 1388903405786}]}'''
+    self.assertEqual(expected, j)
+
+if __name__ == '__main__':
+  unittest.main()
