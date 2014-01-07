8350d056c6da131fe3101de9815be8e30850aa6c
axiomiety <axiomiety@gmail.com>
2014-01-07 21:06:04 +0000

    store locations locally. first draft for batching queries to the distance matrix api

--- bixi/metrics.py
+++ bixi/metrics.py
@@ -9,36 +9,79 @@
 import  listener
 import  urllib.request
 import  json
+import  pickle
+import  os.path
+import  logging
 from    itertools       import  permutations
 from    collections     import  namedtuple, defaultdict
 
+
 Location = namedtuple('Location', ['id', 'name', 'lat', 'long'])
+
+PFILE_LOCATIONS       = 'locations.pickle'
+PFILE_DISTANCE_MATRIX = 'dmatrix.pickle'
+PFILE_BATCHLOADER     = 'batchloader.pickle'
 
 class DistanceMatrix(object):
 
-  def __init__(self, dynamic=True):
-    self.matrix = defaultdict(dict)
-
-    if dynamic: # then we initialise everything from scratch
-      snapshot = json.loads(listener.fetch())
-      self.locations = [ Location(**{ k: loc[k] for k in Location._fields }) for loc in snapshot ]
+  def __init__(self, loadDistanceMatrix=False):
+    self.locations = self.getLocations()
+    if loadDistanceMatrix:
+      self.matrix = self.getDistanceMatrix()
     else:
-      # load from pickled file
-      raise NotImplementedError('not yet implemented')
-    
-    #permutations(self.locations, 2)
+      self.matrix = defaultdict(dict)
 
+  def getLocations(self, cached=True):
+    if cached and os.path.isfile(PFILE_LOCATIONS): # read from local pickle file
+      with open(PFILE_LOCATIONS, 'rb') as f:
+        locations = pickle.load(f)
+    else:
+      snapshot = json.loads(listener.fetch())
+      locations = [ Location(**{ k: loc[k] for k in Location._fields }) for loc in snapshot ]
+      # we went through the trouble of parsing, we might as well save the results
+      with open(PFILE_LOCATIONS, 'wb') as f:
+        pickle.dump(locations, f)
+    
+    return locations  
+      
+  def getDistanceMatrix(self, cached=True):
+    if cached:
+      with open(PFILE_DISTANCE_MATRIX, 'rb') as f:
+        return picke.load(f)
+    else:
+      self.locationsMap = {loc.id: loc for loc in self.locations}
+      # unfortunately dist(A,B) is not necessarily the same as dist(B,A)
+      perms = list(permutations(self.locationsMap.keys(), 2))
+      # the free Distance Matrix only allows a certain number of requests per day
+      # we need to be able to batch those up so we can leave where we left off
+      status = dict()
+      ist = dict()
+      if os.path.isfile(PFILE_BATCHLOADER):
+        with open(PFILE_BATCHLOADER, 'wb') as f:
+          ist = pickle.load(f)  
+      
+      status['remaining_location_pairs'] = ist.get('remaining_location_pairs', perms)
+      
+      dm[a.id][b.id] = fetch(gen_url(a, b))
+    
   @staticmethod
   def fetch(url):
     '''
     data returned is of this format:
     [{'elements': [{'duration': {'text': '11 mins', 'value': 686}, 'distance': {'text': '2.8 km', 'value': 2771}, 'status': 'OK'}]}]
     '''
+    logging.debug('fetching %s' % url)
     req = urllib.request.urlopen(url)
     raw_data = req.read()
     if raw_data:
-      return json.loads(raw_data.decode('utf8'))
-  
+      dr = json.loads(raw_data.decode('utf8'))
+      if dr['status'] == 'OK':
+        e = dr['rows'][0]['elements'][0]
+        return e['duration']['value'] # time is what we're after
+
+    #TODO: log this as an error!
+    return None
+
   @staticmethod
   def gen_url(origin, dest):
     # sample url:
