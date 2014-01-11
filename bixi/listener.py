c9d600115363b4435ff9137dbaffda8d7c754035
axiomiety <axiomiety@gmail.com>
2014-01-11 21:36:30 +0000

    listener, complete dmatrix

--- bixi/listener.py
+++ bixi/listener.py
@@ -3,6 +3,9 @@
 
 import  urllib.request
 import  json
+import  time
+import  logging
+import  os
 from    xml.etree       import ElementTree
 
 PARSING_MAP = {
@@ -14,6 +17,9 @@ PARSING_MAP = {
   'empty_docks' : lambda e: int(e.find('nbEmptyDocks').text),
   'updatets'    : lambda e: int(e.find('latestUpdateTime').text)
   }
+
+POLLING_INTERVAL = 10 # a minute
+UPDATES_DIR = 'updates'
 
 def xml2json(root):
   stations = []
@@ -31,4 +37,25 @@ def fetch():
     xml = ElementTree.fromstring(raw_data)
     return xml2json(xml)
 
-#http://maps.googleapis.com/maps/api/distancematrix/json?origins=43.6633,-79.392891&destinations=43.652123,-79.28758&mode=bicycling&sensor=false
+def poll(delay=None):
+  delay = delay or POLLING_INTERVAL
+  if not os.path.exists(UPDATES_DIR):
+    os.mkdir(UPDATES_DIR)
+  baseline = fetch()
+  running = True
+  while(running):
+    snap = fetch()
+    if snap != baseline: # we have an update
+      logging.info('update found!')
+      o = json.loads(snap)
+      ts = list(o.keys())[0]
+      # save it out!
+      with open(os.path.join(UPDATES_DIR, ts), 'a') as f:
+        f.write(snap)
+      baseline = snap
+
+    time.sleep(delay)
+    logging.info('slept %s seconds. yawn...' % POLLING_INTERVAL)
+
+if __name__ == '__main__':
+  poll()
