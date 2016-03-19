c6a55fdc23362af2f8552dbe145eb38285d7a521
axiomiety <axiomiety@gmail.com>
2016-03-19 03:08:11 +0000

    dotfiles

--- copy_dot_files.sh
+++ copy_dot_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+HOME_DIR=/home/vagrant
 SETUP_DIR=/home/vagrant/setup # this gets run as root : (
-cp -v ${SETUP_DIR}/.vimrc ~/
-cp -v ${SETUP_DIR}/.tmux.conf ~/
-cp -v ${SETUP_DIR}/.inputrc ~/
-cp -v ${SETUP_DIR}/.bashrc ~/
+cp -v ${SETUP_DIR}/.vimrc ${HOME_DIR}
+cp -v ${SETUP_DIR}/.tmux.conf ${HOME_DIR}
+cp -v ${SETUP_DIR}/.inputrc ${HOME_DIR}
+cp -v ${SETUP_DIR}/.bashrc ${HOME_DIR}
+
