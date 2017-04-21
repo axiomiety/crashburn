66d39470b9c8473732513d1fa983fb27fd7a4f61
U0_a91 <axiomiety@gmail.com>
2017-04-21 17:04:07 +0800

    ch5

--- eloquent_js/ch5.js
+++ eloquent_js/ch5.js
@@ -2,4 +2,18 @@ exports.flatten = function (arr) {
   return arr.reduce((acc,val) => acc.concat(val));
 }
 
+exports.every = function(arr, pred) {
+  for (var v in arr) {
+    if (pred(arr[v])) { continue; }
+    return false;
+  }
+  return true;
+}
 
+exports.some = function(arr, pred) {
+  for (var v in arr) {
+    if (pred(arr[v])) { return true; }
+    continue;
+  }
+  return false;
+}
