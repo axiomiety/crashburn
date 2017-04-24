83e23d64bcff4adf52df21c9d860e3c766f6f743
U0_a91 <axiomiety@gmail.com>
2017-04-24 16:28:45 +0800

    ch5

--- eloquent_js/ch5.js
+++ eloquent_js/ch5.js
@@ -18,6 +18,40 @@ exports.some = function(arr, pred) {
   return false;
 }
 
-exports.average = function(arr) {
+function average(arr) {
   return arr.reduce((a,b) => (a+b)) / arr.length;
 }
+
+exports.avg = average;
+
+const ANCESTRY_FILE = require('./ancestry.js');
+var ancestry = JSON.parse(ANCESTRY_FILE);
+var byName = {};
+ancestry.forEach(person => byName[person.name] = person);
+
+exports.mcad = function() {
+  var diffs = new Array;
+  ancestry.forEach(function(p) {
+    if (byName[p.mother]) {
+      diffs.push(p.born - byName[p.mother].born);
+    };
+  });
+  return average(diffs);
+}
+
+function groupBy(arr, fn) {
+  var grouping = {};
+  for (var i in arr) {
+    var key = fn(arr[i]);
+    if (!(key in grouping)) { grouping[key] = new Array(); }
+    grouping[key].push(arr[i]);
+  };
+  return grouping;
+};
+
+exports.gb = groupBy;
+
+exports.le = function() {
+  var g = groupBy(ancestry, function(p) {return Math.ceil(p.died/100);});
+  return Object.keys(g).map(k => average(g[k].map(p => p.died-p.born)));
+};
