6f9ac83e12a3e663540622d047ad84012d8a59b3
axiomiety <axiomiety@gmail.com>
2019-12-07 08:33:29 +0800

    fix bug in hex scale

--- pixelheist/js/lib.js
+++ pixelheist/js/lib.js
@@ -1,24 +1,24 @@
 'use strict';
 
-export function convertHexStringToRgb(str) {
+const convertHexStringToRgba = (str) => {
     const decimalValue = parseInt(str,16);
     const paletteWidth = 47;
     let val = decimalValue*paletteWidth;
     const color = {r: 0, g: 0, b: 0};
     if (val > 255*2) {
       color.b = Math.round(val - 255*2);
-      val -= 255*2;
-    }
-    if (val > 255) {
+      color.r, color.g = 255;
+    } else if (val > 255) {
       color.g = Math.round(val - 255);
-      val -= 255;
-    }
-    color.r = Math.round(val);
+      color.r = 255;
+    } else {
+      color.r = Math.round(val);
+    }    
     return `rgba(${color.r}, ${color.g}, ${color.b}, 1)`;
     //return '#FF0000';
   }
   
-  export function formatStringToIndividualHexNumbers(str) {
+  const formatByteStringToHexNumbersArray = (str) => {
     const ret = [];
     if (str.length == 1) {
       ret.push('0', str);
@@ -27,3 +27,6 @@ export function convertHexStringToRgb(str) {
     }
     return ret;
   }
+
+  exports.convertHexStringToRgba = convertHexStringToRgba;
+  exports.formatByteStringToHexNumbersArray = formatByteStringToHexNumbersArray;
--- pixelheist/js/test/utils.js
+++ pixelheist/js/test/utils.js
@@ -1,9 +1,16 @@
 const wish = require('wish');
 //import convertHexStringToRgb from '../index.js';
-const { convertHexStringToRgb } = require('../lib.js');
+const { convertHexStringToRgba } = require('../lib.js');
 
-describe('foo', function() {
-    it('bar', function() {
-
+describe('utilities', function() {
+    it('converts a hexadecimal string (0-f) to an RGB triplet', function() {
+        let ret = convertHexStringToRgba('0');
+        wish( 'rgba(0, 0, 0, 1)' === ret);
+        ret = convertHexStringToRgba('1');
+        console.log(ret);
+        wish( 'rgba(47, 0, 0, 1)' === ret);
+        ret = convertHexStringToRgba('6');
+        console.log(ret);
+        wish( 'rgba(255, 27, 0, 1)' === ret); 
     });
 });
