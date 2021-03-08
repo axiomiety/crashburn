fa92c73dda8f93bc3074508dcda3e359bde14ad4
axiomiety <axiomiety@gmail.com>
2021-03-08 22:50:51 +0800

    update

--- leetcode/binary_search.py
+++ leetcode/binary_search.py
@@ -5,9 +5,9 @@ class Solution:
             mid = start + (end-start) // 2
             val = nums[mid]
             if val > target:
-                end = mid-1 if end == mid else mid
+                end = mid-1
             elif val < target:
-                start = mid+1 if start == mid else mid
+                start = mid+1
             else:
                 return mid
         return -1
