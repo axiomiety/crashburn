1f763fafa7b70eca398139300f6428d1b763be69
axiomiety <axiomiety@gmail.com>
2021-04-04 17:28:48 +0800

    update

--- leetcode/find_peak_element.py
+++ leetcode/find_peak_element.py
@@ -19,3 +19,32 @@ class Solution:
             if is_peak(i):
                 return i
         return -1
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        if not nums:
+            return -1
+        if len(nums) == 1:
+            return 0
+
+        def is_peak(idx):
+            if idx == 0:
+                return nums[idx] > nums[idx+1]
+            elif idx == len(nums)-1:
+                return nums[idx] > nums[idx-1]
+            else:
+                return nums[idx-1] < nums[idx] and nums[idx] > nums[idx+1]
+            
+        start, end = 0, len(nums)-1
+        while start < end:
+            mid = start + (end-start)//2
+            if is_peak(mid):
+                return mid
+            elif mid > 0:
+                if nums[mid-1] < nums[mid] < nums[mid+1]:
+                    start = mid+1
+                else:
+                    end = mid-1
+            else:
+                return 0 if nums[0] > nums[1] else 1
+        
+        return start
