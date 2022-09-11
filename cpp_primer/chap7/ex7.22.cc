c94c173461bfef70ad7ff59965fb4dd0ae00a8bf
axiomiety <axiomiety@gmail.com>
2022-09-11 19:24:28 +0800

    c++ primer: chapter 7

---
+++ cpp_primer/chap7/ex7.22.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+using std::begin;
+using std::cin;
+using std::cout;
+using std::end;
+using std::endl;
+using std::initializer_list;
+using std::map;
+using std::ostream;
+using std::string;
+using std::vector;
+
+struct Person
+{
+    friend ostream &print(Person p, ostream &os);
+
+public:
+    Person(char *name);
+    Person(string name);
+    Person(string name, string address) : name(name), address(address) {}
+    string getName() const
+    {
+        return name;
+    }
+    string getAddress() const
+    {
+        return address;
+    }
+    ostream &print(ostream &os);
+
+private:
+    string name;
+    string address;
+};
+
+// just for friends
+ostream &print(Person p, ostream &os)
+{
+    p.print(os);
+    return os;
+}
+
+ostream &Person::print(ostream &os)
+{
+    os << name << " lives at: " << address;
+    return os;
+}
+
+// note that if the arg is called `name`, this will
+// mask the local definition of the `name` member variable!
+Person::Person(string n)
+{
+    cout << "string constructor" << endl;
+    name = n;
+}
+
+Person::Person(char *name) : Person(string(name), "N/A")
+{
+    cout << "char* constructor" << endl;
+}
+
+int main(int argc, char **argv)
+{
+    Person p = Person("santa", "north pole");
+    p.print(cout) << endl;
+    Person p2 = Person(string("Easter bunny"));
+    p2.print(cout) << endl;
+    Person p3 = Person(string("tooth fairy").c_str());
+    print(p3, cout) << endl;
+    return EXIT_SUCCESS;
+}
---
+++ cpp_primer/chap7/ex7.23.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+using std::begin;
+using std::cin;
+using std::cout;
+using std::end;
+using std::endl;
+using std::initializer_list;
+using std::map;
+using std::ostream;
+using std::string;
+using std::vector;
+
+class Screen
+{
+public:
+    typedef std::string::size_type pos;
+    Screen() = default;
+    Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c) {}
+    Screen(pos ht, pos wd) : Screen(ht, wd, 0) {}
+    char get() const { return contents[cursor]; }
+    inline char get(pos ht, pos wd) const;
+    Screen &move(pos r, pos c);
+
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    string contents;
+};
+
+inline Screen &Screen::move(pos r, pos c)
+{
+    pos row = r * width;
+    cursor = row + c;
+    return *this;
+}
+char Screen::get(pos r, pos c) const
+{
+    pos row = r * width;
+    return contents[row + c];
+}
+
+int main(int argc, char **argv)
+{
+    return EXIT_SUCCESS;
+}
