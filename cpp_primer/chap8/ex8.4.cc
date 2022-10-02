5188b26ee2f0a68b53fc64840484f4eb82f6db98
axiomiety <axiomiety@gmail.com>
2022-10-02 21:35:16 +0800

    c++ primer: chapter 8

---
+++ cpp_primer/chap8/ex8.4.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::end;
+using std::endl;
+using std::getline;
+using std::ifstream;
+using std::initializer_list;
+using std::istream;
+using std::map;
+using std::string;
+using std::vector;
+
+void my_read(string fname, vector<string> *svec)
+{
+    string line;
+    ifstream input(fname);
+    if (input)
+    {
+        while (getline(input, line))
+        {
+            svec->push_back(line);
+        }
+    }
+    else
+    {
+        cerr << "could not open " + fname << " for reading" << endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    vector<string> *svec = new vector<string>();
+
+    my_read(string(argv[1]), svec);
+    for (const auto &line : *svec)
+    {
+        cout << line << endl;
+    }
+    return EXIT_SUCCESS;
+}
