5162648da6f0a7abe07cf917e19f80dafd637a53
axiomiety <axiomiety@gmail.com>
2022-10-22 15:00:59 +0800

    c++ primer: chapter 9

---
+++ cpp_primer/chap9/ex9.43.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::deque;
+using std::end;
+using std::endl;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+
+void search_and_replace(string &s, const string &old_val, const string &new_val)
+{
+}
+
+int main(int argc, char **argv)
+{
+    char cstr[] = "Hello";
+    vector<char> cvec(begin(cstr), end(cstr));
+    string s(cstr);
+    string ss(cvec.cbegin(), cvec.cend());
+    cout << s << endl;
+    cout << ss << endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ cpp_primer/chap9/prog3.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::deque;
+using std::end;
+using std::endl;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+
+int main(int argc, char **argv)
+{
+    vector<int> ivec = {0, 1, 1, 2, 3, 5, 8, 13, 21, 55, 89};
+
+    cout << "capacity: " << ivec.capacity() << endl;
+    auto iter = ivec.begin();
+    while (iter != ivec.end())
+    {
+        if (*iter % 2)
+            iter = ivec.insert(iter, *iter) + 1;
+        ++iter;
+    }
+
+    cout << "capacity: " << ivec.capacity() << endl;
+    for (const auto &it : ivec)
+        cout << it << " ";
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
