ed66e0c445bc78efdc0696e738c9fc27a4490ac9
axiomiety <axiomiety@gmail.com>
2022-10-24 22:01:54 +0800

    c++ primer: chapter 9

--- cpp_primer/chap9/ex9.43.cc
+++ cpp_primer/chap9/ex9.43.cc
@@ -34,16 +34,42 @@ using std::vector;
 
 void search_and_replace(string &s, const string &old_val, const string &new_val)
 {
+    auto s_iter = s.begin();
+    auto match_start = old_val.cend();
+    auto old_val_iter = old_val.cbegin();
+    while (s_iter != s.end())
+    {
+        cout << "s_iter is: " << *s_iter << endl;
+        if (*s_iter == *old_val_iter)
+        {
+            if (match_start == old_val.cend())
+                match_start = s_iter;
+            ++s_iter;
+            ++old_val_iter;
+        }
+        else
+        {
+            if (old_val_iter == old_val.cend())
+            {
+                // full match found
+                // cout << "before: " << s << endl;
+                s_iter = s.erase(match_start, match_start + old_val.size());
+                // cout << "after: " << s << endl;
+                s_iter = s.insert(s_iter, new_val.begin(), new_val.end()); // new_val);
+                // cout << "insert: " << s << endl;
+                s_iter += new_val.size();
+            }
+            match_start = old_val.cend();
+            ++s_iter;
+            old_val_iter = old_val.cbegin();
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
-    char cstr[] = "Hello";
-    vector<char> cvec(begin(cstr), end(cstr));
-    string s(cstr);
-    string ss(cvec.cbegin(), cvec.cend());
+    string s("the quick brown fox jumps over the lazy dog");
+    search_and_replace(s, "the", "foo");
     cout << s << endl;
-    cout << ss << endl;
-
     return EXIT_SUCCESS;
 }
