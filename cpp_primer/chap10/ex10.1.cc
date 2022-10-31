e782d65c2bb3a3eb7a08da5a9534bf8190a25ee4
axiomiety <axiomiety@gmail.com>
2022-10-31 21:00:08 +0800

    c++ primer: chapter 10

---
+++ cpp_primer/chap10/ex10.1.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::deque;
+using std::end;
+using std::endl;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+
+int main(int argc, char **argv)
+{
+    vector<int> ivec;
+    string token;
+    while (cin >> token)
+    {
+        ivec.push_back(stoi(token));
+    }
+    for (auto i : {1, 2, 3})
+    {
+        cout << i << ": " << std::count(ivec.cbegin(), ivec.cend(), i) << endl;
+    }
+    return EXIT_SUCCESS;
+}
