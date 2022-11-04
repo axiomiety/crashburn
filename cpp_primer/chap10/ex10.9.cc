bf5b3a64503d2747dbd98d4a8e5c2a9a6560e8f0
axiomiety <axiomiety@gmail.com>
2022-11-05 07:26:49 +0800

    c++ primer: chapter 10

---
+++ cpp_primer/chap10/ex10.9.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+using std::back_inserter;
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::deque;
+using std::end;
+using std::endl;
+using std::fill_n;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+
+int main(int argc, char **argv)
+{
+    vector<int> ivec;
+    string token;
+    while (cin >> token)
+        ivec.push_back(std::stoi(token));
+
+    sort(ivec.begin(), ivec.end());
+    auto end_unique = unique(ivec.begin(), ivec.end());
+    cout << "before erasing: " << endl;
+    for (const auto &i : ivec)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    ivec.erase(end_unique, ivec.end());
+    cout << "after erasing: " << endl;
+    for (const auto &i : ivec)
+    {
+        cout << i << " ";
+    }
+    return EXIT_SUCCESS;
+}
