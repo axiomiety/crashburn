b8f2776adc47aef869ebc7e5c12b7a8227856f45
axiomiety <axiomiety@gmail.com>
2022-11-13 21:22:58 +0800

    c++ primer: chapter 10

---
+++ cpp_primer/chap10/ex10.29.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+using std::back_inserter;
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::deque;
+using std::end;
+using std::endl;
+using std::fill_n;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+using namespace std::placeholders;
+
+int main(int argc, char **argv)
+{
+    vector<string> svec;
+    std::ifstream in("ex10.29.cc");
+    std::istream_iterator<string> str_in(in), eof;
+    std::copy(str_in, eof, std::back_inserter(svec));
+
+    std::ostream_iterator<string> out_iter(cout, " ");
+    std::copy(svec.begin(), svec.end(), out_iter);
+    cout << endl;
+    return EXIT_SUCCESS;
+}
