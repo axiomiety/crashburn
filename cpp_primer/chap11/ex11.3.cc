71fafcaa9da77ae4aa84f38eab7baa266965d5a7
axiomiety <axiomiety@gmail.com>
2022-11-21 21:14:04 +0800

    c++ primer: chapter 11

---
+++ cpp_primer/chap11/ex11.3.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+
+using std::back_inserter;
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::deque;
+using std::end;
+using std::endl;
+using std::fill_n;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::ofstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+using namespace std::placeholders;
+
+int main(int argc, char **argv)
+{
+    map<string, size_t> smap;
+    string word;
+    while (cin >> word)
+        ++smap[word];
+    
+    vector<string> words;
+    for ( const auto &[key, value]: smap ) 
+        words.push_back(key);
+    
+    std::sort(words.begin(), words.end());
+
+    for ( auto const &w: words)
+        cout << "w: " << w << ": " << smap[w] << endl;
+
+    return EXIT_SUCCESS;
+}
