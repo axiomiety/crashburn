030011b900d82c0ac54e6a4b8b2b139db7dd02e6
axiomiety <axiomiety@gmail.com>
2022-12-06 21:51:20 +0800

    c++ primer: chapter 12

---
+++ cpp_primer/chap12/prog.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <cassert>
+#include <memory>
+
+using std::back_inserter;
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::deque;
+using std::end;
+using std::endl;
+using std::fill_n;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::ofstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::multimap;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+using namespace std::placeholders;
+using std::exception;
+using std::make_shared;
+using std::shared_ptr;
+
+shared_ptr<vector<int>>
+build_on_heap(int val)
+{
+    return make_shared<vector<int>>(val, 5);
+}
+
+int main(int argc, char **argv)
+{
+    auto ivec = build_on_heap(std::stoi(argv[1]));
+    for (auto const& v: *ivec)
+        cout << v << ",";
+    cout << endl;
+    return EXIT_SUCCESS;
+}
