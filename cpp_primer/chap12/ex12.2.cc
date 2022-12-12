6066df691b4444495e7f6014f429c53f68ea9990
axiomiety <axiomiety@gmail.com>
2022-12-12 21:27:43 +0800

    c++ primer: chapter 12

---
+++ cpp_primer/chap12/ex12.2.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <cassert>
+#include <memory>
+
+using std::back_inserter;
+using std::begin;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::deque;
+using std::end;
+using std::endl;
+using std::fill_n;
+using std::forward_list;
+using std::getline;
+using std::ifstream;
+using std::ofstream;
+using std::initializer_list;
+using std::istream;
+using std::istringstream;
+using std::iterator;
+using std::list;
+using std::map;
+using std::multimap;
+using std::ostream;
+using std::ostringstream;
+using std::string;
+using std::vector;
+using namespace std::placeholders;
+using std::exception;
+using std::shared_ptr;
+using std::make_shared;
+
+class StrBlob {
+    public:
+        typedef vector<string>::size_type size_type;
+        StrBlob();
+        StrBlob(initializer_list<string> il);
+        size_type size() const { return data->size(); }
+        bool empty() const { return data->empty(); }
+        void push_back(const string& t) { data->push_back(t); }
+        void pop_back();
+        string& front();
+        string & back();
+    private:
+        shared_ptr<vector<string>> data;
+        void check(size_type i, const string& msg) const;
+};
+
+StrBlob::StrBlob(): data(make_shared<vector<string>>()) { }
+StrBlob::StrBlob(initializer_list<string> il): data(make_shared<vector<string>>(il)) { }
+
+int main(int argc, char **argv)
+{
+    return EXIT_SUCCESS;
+}
