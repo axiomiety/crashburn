e5dc7cdfa9c14f50ca39bf92bd41c2677a535651
axiomiety <axiomiety@gmail.com>
2022-12-14 21:35:31 +0800

    c++ primer: chapter 12

--- cpp_primer/chap12/ex12.2.cc
+++ cpp_primer/chap12/ex12.2.cc
@@ -68,6 +68,30 @@ class StrBlob {
 StrBlob::StrBlob(): data(make_shared<vector<string>>()) { }
 StrBlob::StrBlob(initializer_list<string> il): data(make_shared<vector<string>>(il)) { }
 
+void StrBlob::check(size_type i, const string& msg) const
+{
+    if (i >= data->size())
+        throw std::out_of_range(msg);
+}
+
+string& StrBlob::front()
+{
+    check(0, "front on emtpy StrBlobl");
+    return data->front();
+}
+
+string& StrBlob::back()
+{
+    check(0, "back on emtpy StrBlobl");
+    return data->back();
+}
+
+void StrBlob::pop_back()
+{
+    check(0, "pop_back on empty StrBlob");
+    data->pop_back();
+}
+
 int main(int argc, char **argv)
 {
     return EXIT_SUCCESS;
