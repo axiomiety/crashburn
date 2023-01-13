e95f0d35168101ddb90af40bda266d2bd12a09ec
axiomiety <axiomiety@gmail.com>
2023-01-13 21:34:13 +0800

    c++ primer: chapter 13

---
+++ cpp_primer/chap13/ex13.5.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <cassert>
+#include <memory>
+
+using namespace std;
+
+class HasPtr
+{
+    public:
+        HasPtr(const string &s= string()): ps(new string(s)), i(0) {}
+        HasPtr(const HasPtr& c) : ps(new string(*c.ps)), i(c.i) {}
+        void print() { cout << ps << "," << *ps << "," << i << endl;}
+    private:
+        string* ps;
+        int i;
+};
+
+int main(int argc, char **argv)
+{
+    const string s("foobar");
+    auto a = HasPtr(s);
+    auto b = HasPtr(a);
+
+    a.print();
+    b.print();
+
+    return EXIT_SUCCESS;
+}
