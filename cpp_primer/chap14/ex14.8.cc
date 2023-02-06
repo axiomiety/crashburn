beb2516f10033199566de41b9f7cb4cbd9b50cb9
axiomiety <axiomiety@gmail.com>
2023-02-06 21:33:00 +0800

    c++ primer: chapter 14

---
+++ cpp_primer/chap14/ex14.8.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <cassert>
+#include <memory>
+
+using namespace std;
+
+class Foo
+{
+public:
+    Foo(vector<int> data) : data(data) { }
+    Foo sorted() &&;
+    Foo sorted() const &;
+    string toString() const;
+    ostream& operator<<(ostream &os, const Foo &item);
+private:
+    vector<int> data;
+};
+
+ostream& operator<<(ostream &os, const Foo &item {
+    os << 
+    return os;
+}
+
+Foo
+Foo::sorted() &&
+{
+    cout << "rval sort" << endl;
+    sort(data.begin(), data.end());
+    return *this;
+}
+Foo Foo::sorted() const& {
+    cout << "lval sort" << endl;
+    Foo ret(*this);
+    sort(ret.data.begin(), ret.data.end());
+    return ret;
+}
+
+string Foo::toString() const {
+}
+
+int main(int argc, char **argv)
+{
+    auto f1 = Foo({1,2,3});
+    f1.sorted();
+    cout << f1.toString() << endl;
+    cout << Foo({3,2,1}).sorted().toString() << endl;
+    return EXIT_SUCCESS;
+}
