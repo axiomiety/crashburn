161a8deff0950a0f90fb3cb198978edb1146a894
axiomiety <axiomiety@gmail.com>
2023-03-04 05:04:28 -0500

    c++ primer: chapter 14

---
+++ cpp_primer/chap14/ex14.42.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <forward_list>
+#include <stdexcept>
+#include <map>
+#include <initializer_list>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <cassert>
+#include <memory>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    vector<int> ivec = {10, 2000, 50, 60, 1500};
+    auto c = count_if(ivec.cbegin(), ivec.cend(), [](int a){ return a > 1024; });
+    cout << c << endl;
+
+    vector<string> svec = {"pooh", "pooh", "bear", "honey"};
+    auto it = find_if_not(svec.cbegin(), svec.cend(), [](string s){ return s == "pooh";});
+    cout << *it << endl;
+
+    vector<int> nivec;
+    transform(ivec.cbegin(), ivec.cend(), back_inserter(nivec), [](int a){ return a*2;});
+    for (const auto i: nivec)
+        cout << i << " ";
+    cout << endl;
+    return EXIT_SUCCESS;
+}
