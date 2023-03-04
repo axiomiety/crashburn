05d77a66804d5ab623b3d41ab13380132871276d
axiomiety <axiomiety@gmail.com>
2023-03-04 05:12:09 -0500

    c++ primer: chapter 14

--- cpp_primer/chap14/ex14.42.cc
+++ cpp_primer/chap14/ex14.42.cc
@@ -26,6 +26,8 @@ int main(int argc, char **argv)
     vector<int> ivec = {10, 2000, 50, 60, 1500};
     auto c = count_if(ivec.cbegin(), ivec.cend(), [](int a){ return a > 1024; });
     cout << c << endl;
+    # we can use bind too, but as i understand it the advice is to replace this with lambda nowadays
+    cout << count_if(ivec.cbegin(), ivec.cend(), bind(greater<int>(), placeholders::_1, 1024)) << endl;
 
     vector<string> svec = {"pooh", "pooh", "bear", "honey"};
     auto it = find_if_not(svec.cbegin(), svec.cend(), [](string s){ return s == "pooh";});
