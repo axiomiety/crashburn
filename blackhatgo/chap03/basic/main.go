fe1d3bc442b18669c723de175e8c7f95c3f8bf7b
axiomiety <axiomiety@gmail.com>
2023-05-09 21:36:03 +0800

    blackhat go - chap3

---
+++ blackhatgo/chap03/basic/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://www.google.com/robots.txt")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	fmt.Println(resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println(string(body))
+	resp.Body.Close()
+}
