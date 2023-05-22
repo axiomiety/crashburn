d199186b9a55b36b03c7f509afb3a4b775e511e6
axiomiety <axiomiety@gmail.com>
2023-05-22 15:11:10 +0800

    blackhat go - chap4

--- blackhatgo/chap04/go.mod
+++ blackhatgo/chap04/go.mod
@@ -4,5 +4,8 @@ go 1.20
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/sirupsen/logrus v1.9.2 // indirect
 	github.com/urfave/negroni v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
---
+++ blackhatgo/chap04/ws-klogger/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+
+	listenAddr string
+	wsAddr     string
+	jsTemplate *template.Template
+)
+
+func init() {
+	flag.StringVar(&listenAddr, "listen-addr", "", "address to listen on")
+	flag.StringVar(&wsAddr, "ws-addr", "", "address for websocket connection")
+	flag.Parse()
+
+	var err error
+	jsTemplate, err = template.ParseFiles("ws-klogger/logger.js")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func serveWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "", 500)
+		return
+	}
+
+	defer conn.Close()
+	fmt.Printf("connection from %s\n", conn.RemoteAddr().String())
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		fmt.Printf("from %s: %s\n", conn.RemoteAddr().String(), string(msg))
+	}
+}
+
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	jsTemplate.Execute(w, wsAddr)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/ws", serveWS)
+	r.HandleFunc("/k.js", serveFile)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
