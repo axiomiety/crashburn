1902e8d1fd8b0778ae4b12e7a4dfe97537cbdb5e
axiomiety <axiomiety@gmail.com>
2023-05-27 20:49:22 +0800

    blackhat go - chap5

--- blackhatgo/chap05/subd-guesser/main.go
+++ blackhatgo/chap05/subd-guesser/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"bufio"
 	"errors"
 	"flag"
+	"fmt"
 	"github.com/miekg/dns"
+	"os"
+	"text/tabwriter"
 )
 
 type result struct {
@@ -52,6 +54,40 @@ func lookupCNAME(fqdn, serverAddr string) ([]string, error) {
 	return fqdns, nil
 }
 
+func lookup(fqdn, serverAddr string) []result {
+	var results []result
+	var cfqdn = fqdn // leave the input alone
+	for {
+		cnames, err := lookupCNAME(cfqdn, serverAddr)
+		if err == nil && len(cnames) > 0 {
+			cfqdn = cnames[0]
+			continue
+		}
+		ips, err := lookupA(cfqdn, serverAddr)
+		if err != nil {
+			break
+		}
+		for _, ip := range ips {
+			results = append(results, result{IPAddress: ip, Hostname: fqdn})
+		}
+		break
+	}
+	return results
+}
+
+type empty struct{}
+
+func worker(tracker chan empty, fqdns chan string, gather chan []result, serverAddr string) {
+	for fqdn := range fqdns {
+		results := lookup(fqdn, serverAddr)
+		if len(results) > 0 {
+			gather <- results
+		}
+	}
+	var e empty
+	tracker <- e
+}
+
 func main() {
 	var (
 		flDomain      = flag.String("domain", "", "the domain to perform guesses against")
@@ -66,4 +102,45 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println(*flWorkerCount, *flServerAddr)
+
+	var results []result
+	fqdns := make(chan string, *flWorkerCount)
+	gather := make(chan []result)
+	tracker := make(chan empty)
+
+	fh, err := os.Open(*flWordlist)
+	if err != nil {
+		panic(err)
+	}
+	defer fh.Close()
+	scanner := bufio.NewScanner(fh)
+
+	for i := 0; i < *flWorkerCount; i++ {
+		go worker(tracker, fqdns, gather, *flServerAddr)
+	}
+
+	for scanner.Scan() {
+		fqdns <- fmt.Sprintf("%s.%s", scanner.Text(), *flDomain)
+	}
+
+	go func() {
+		for r := range gather {
+			results = append(results, r...)
+		}
+		var e empty
+		tracker <- e
+	}()
+
+	close(fqdns)
+	for i := 0; i < *flWorkerCount; i++ {
+		<-tracker
+	}
+	close(gather)
+	<-tracker
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 8, 4, ' ', 0)
+	for _, r := range results {
+		fmt.Fprintf(w, "%s\t%s\n", r.Hostname, r.IPAddress)
+	}
+	w.Flush()
 }
