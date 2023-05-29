50cbe3b7b48a64f461b82e6dea5ffa83ee796552
axiomiety <axiomiety@gmail.com>
2023-05-29 17:33:26 +0800

    blackhat go - chap5

---
+++ blackhatgo/chap08/identify/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	for _, device := range devices {
+		fmt.Println(device.Name)
+		for _, address := range device.Addresses {
+			fmt.Printf("\tIP:\t%s\n", address.IP)
+			fmt.Printf("\tNetmask:\t%s\n", address.Netmask)
+		}
+	}
+}
