58fe1d741986a44e7a79d7da87f54cf0eb77e1bf
axiomiety <axiomiety@gmail.com>
2023-07-07 11:55:10 +0800

    bt-101

--- go-bt/src/data/protocol.go
+++ go-bt/src/data/protocol.go
@@ -6,7 +6,6 @@ import (
 )
 
 type Handshake struct {
-	Pstrlen byte
 	Pstr string
 	Reserved [8]byte
 	InfoHash [20]byte
@@ -19,9 +18,29 @@ func GetHanshake(infoHash [20]byte, peerId [20]byte) Handshake {
 		InfoHash : infoHash,
 		PeerId : peerId,
 	}
-	buffer := new(bytes.Buffer)
-	binary.Write(buffer, binary.BigEndian, len(handshake.Pstr))
-	handshake.Pstrlen = buffer.Bytes()[0]
 
 	return handshake
 }
+
+func (h *Handshake) ToBytes() []byte {
+	buffer := new(bytes.Buffer)
+	// 4 bytes
+	b := make([]byte,4)
+	binary.BigEndian.PutUint32(b, uint32(len(h.Pstr)))
+	buffer.Write([]byte(h.Pstr))
+	buffer.Write(h.Reserved[:])
+	buffer.Write(h.InfoHash[:])
+	buffer.Write(h.PeerId[:])
+	return buffer.Bytes()
+}
+
+/*
+
+ - find a piece we don't have
+ - handshake with a random peer
+ - check state of peer (choked/unchocked)
+ 	- this might be a random delay like 1s?
+	- if choked, move to the next peer
+ - request piece
+
+*/
