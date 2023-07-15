db7a055a5979c16f0d290e1dea792892ae5b5691
axiomiety <axiomiety@gmail.com>
2023-07-15 15:27:54 +0800

    bt-101

--- go-bt/src/data/protocol.go
+++ go-bt/src/data/protocol.go
@@ -3,20 +3,27 @@ package data
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/gob"
 )
 
 type Handshake struct {
-	Pstr string
+	PstrLen  [4]byte
+	Pstr     []byte
 	Reserved [8]byte
 	InfoHash [20]byte
-	PeerId [20]byte
+	PeerId   [20]byte
 }
 
 func GetHanshake(infoHash [20]byte, peerId [20]byte) Handshake {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, uint32(len("BitTorrent Protocol")))
+	pstrLen := [4]byte{}
+	copy(pstrLen[:], b)
 	handshake := Handshake{
-		Pstr : "BitTorrent protocol",
-		InfoHash : infoHash,
-		PeerId : peerId,
+		PstrLen:  pstrLen,
+		Pstr:     []byte("BitTorrent protocol"),
+		InfoHash: infoHash,
+		PeerId:   peerId,
 	}
 
 	return handshake
@@ -24,14 +31,30 @@ func GetHanshake(infoHash [20]byte, peerId [20]byte) Handshake {
 
 func (h *Handshake) ToBytes() []byte {
 	buffer := new(bytes.Buffer)
-	// 4 bytes
-	b := make([]byte,4)
-	binary.BigEndian.PutUint32(b, uint32(len(h.Pstr)))
-	buffer.Write([]byte(h.Pstr))
-	buffer.Write(h.Reserved[:])
-	buffer.Write(h.InfoHash[:])
-	buffer.Write(h.PeerId[:])
+	enc := gob.NewEncoder(buffer)
+	enc.Encode(h)
 	return buffer.Bytes()
+}
+
+type Message struct {
+	Length    [4]byte
+	MessageId byte
+	Payload   []byte
+}
+
+func Request(index uint32, offset uint32, length uint32) Message {
+	lenBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(lenBytes, 13)
+	payload := make([]byte, 4)
+	payload = binary.BigEndian.AppendUint32(payload, index)
+	payload = binary.BigEndian.AppendUint32(payload, offset)
+	payload = binary.BigEndian.AppendUint32(payload, length)
+	msg := Message{
+		Length:    [4]byte(lenBytes),
+		MessageId: 6,
+		Payload:   payload,
+	}
+	return msg
 }
 
 /*
