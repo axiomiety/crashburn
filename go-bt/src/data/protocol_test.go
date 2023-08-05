0587aa5f313d6caa866374865944005d947509d9
axiomiety <axiomiety@gmail.com>
2023-08-05 17:36:26 +0800

    bt-101

---
+++ go-bt/src/data/protocol_test.go
@@ -0,0 +1,44 @@
+package data_test
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"go-bt/data"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/marksamman/bencode"
+)
+
+func TestGetHandshake(t *testing.T) {
+	file, err := os.Open("testdata/ubuntu.torrent")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	dict, err := bencode.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+	infoHash := sha1.Sum(bencode.Encode(dict["info"]))
+	var peerId [20]byte
+	copy(peerId[:], "12345678901234567890")
+	handshake := data.GetHanshake(infoHash, peerId)
+	expectedHexBytes := "13426974546f7272656e742070726f746f636f6c00000000000000009e638562ab1c1fced9def142864cdd5a7019e1aa3132333435363738393031323334353637383930"
+	if hexBytes := hex.EncodeToString(handshake.ToBytes()); hexBytes != expectedHexBytes {
+		t.Errorf("%v", hexBytes)
+	}
+}
+
+func TestGetPiecesFromBitField(t *testing.T) {
+	bitfield := []byte{128}
+	pieces := data.GetPiecesFromBitField(bitfield)
+	expected := map[uint32]bool{0: true}
+	if !reflect.DeepEqual(pieces, expected) {
+		for key := range pieces {
+			t.Errorf("key: %d", key)
+		}
+		t.Errorf("only expecting the first piece")
+	}
+}
---
+++ go-bt/src/data/utils.go
@@ -0,0 +1,58 @@
+package data
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strconv"
+)
+
+func ExtractPiecesFromBitfield(bitfield []byte) map[uint32]bool {
+	pieces := make(map[uint32]bool)
+	for idx, byte_ := range bitfield {
+		for i := 0; i <= 7; i++ {
+			if byte_&byte(2^i) > 0 {
+				pieces[uint32(idx*8+i)] = true
+			}
+		}
+	}
+	return pieces
+}
+
+func WritePiece(pieceIdx uint32, data []byte) {
+	u, _ := user.Current()
+	//TODO: needs another layer of indirection o_O
+	fname := fmt.Sprintf("%s/tmp/blocks/%d", u.HomeDir, pieceIdx)
+	err := os.WriteFile(fname, data, 0644)
+	check(err)
+	log.Printf("wrote piece %s\n", fname)
+}
+
+func GetAlreadyDownloadedPieces(path string, piecesHash string) map[uint32]bool {
+	m := map[uint32]bool{}
+	err := filepath.WalkDir("~/tmp/blocks", func(path string, entry fs.DirEntry, err error) error {
+		if !entry.IsDir() {
+			pieceIdx_, err := strconv.ParseUint(entry.Name(), 10, 32)
+			pieceIdx := uint32(pieceIdx_)
+			check(err)
+			data, err := os.ReadFile(path)
+			check(err)
+			startIdx := 20 * pieceIdx
+			pieceHash := sha1.Sum(data)
+			if bytes.Equal([]byte(pieceHash[:]), []byte(piecesHash[startIdx:startIdx+20])) {
+				log.Printf("piece %d checks out\n", pieceIdx)
+				m[pieceIdx] = true
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("impossible to walk directories: %s", err)
+	}
+	return m
+}
