8dbfa649af1bb1741da5cbcb700a733924ca9110
axiomiety <axiomiety@gmail.com>
2023-11-05 20:51:48 +0800

    [go-bt] another pass at bencode

--- go-bt/src/data/bencode.go
+++ go-bt/src/data/bencode.go
@@ -29,18 +29,38 @@ func readUntilE(r *bufio.Reader) []byte {
 	}
 }
 
-func ParseBencoded(r io.Reader) {
+type Container struct {
+	List []interface{}
+	Dict map[string]interface{}
+}
+
+func (c *Container) add(value interface{}) {
+	c.List = append(c.List, value)
+}
+
+type Curried func(val interface{}) interface{}
+
+/*
+would some sort of stack-based approach work best?
+*/
+func ParseBencoded(r io.Reader) interface{} {
 	reader := bufio.NewReader(r)
-	for {
+	var container interface{}
+
+	parse = func(reader *bufio.Reader) interface{} {
 		b, err := reader.ReadByte()
-		check(err)
+		if err != nil {
+			log.Printf("end reached")
+		}
 		switch b {
+		case 'e':
 		case 'i':
 			val, err := strconv.Atoi(string(readUntilE(reader)))
 			check(err)
-			log.Printf("found int: %d", val)
-			break
+
+		case 'l':
+
 		}
-		break
 	}
+	return container
 }
--- go-bt/src/data/bencode_test.go
+++ go-bt/src/data/bencode_test.go
@@ -7,6 +7,32 @@ import (
 )
 
 func TestBencodeParsing(t *testing.T) {
+
+	// single integer
 	r := bytes.NewReader([]byte("i42e"))
-	data.ParseBencoded(r)
+	ret := data.ParseBencoded(r)
+	if ret != 42 {
+		t.Errorf("expected 42, got %v", ret)
+	}
+
+	// string
+	r = bytes.NewReader([]byte("12:foobarraboof"))
+	ret = data.ParseBencoded(r)
+	if ret != 42 {
+		t.Errorf("expected [42], got %v", ret)
+	}
+
+	// list with one int
+	r = bytes.NewReader([]byte("li42ee"))
+	ret = data.ParseBencoded(r)
+	if ret != 42 {
+		t.Errorf("expected [42], got %v", ret)
+	}
+
+	// list with one item
+	r = bytes.NewReader([]byte("li42ee"))
+	ret = data.ParseBencoded(r)
+	if ret != 42 {
+		t.Errorf("expected [42], got %v", ret)
+	}
 }
