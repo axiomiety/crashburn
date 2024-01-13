efacce99ae20cf49a6565cb37a550609ab0c7996
axiomiety <axiomiety@gmail.com>
2024-01-14 06:23:35 +0800

    [go-bt] better bencode

---
+++ go-bt/src/data/bencode2.go
@@ -0,0 +1,141 @@
+package data
+
+import (
+	"bufio"
+	"io"
+	"strconv"
+)
+
+type Holder interface {
+	Add(value interface{})
+	Obj() interface{}
+	Empty() bool
+}
+
+type ListHolder struct {
+	List []interface{}
+}
+
+type DictHolder struct {
+	Dict map[string]interface{}
+	// tracks the current key
+	Key string
+}
+
+type DictItemHolder struct {
+	Key string
+	Val interface{}
+}
+
+type ValueHolder struct {
+	Val interface{}
+}
+
+// Add methods
+
+func (c *ListHolder) Add(value interface{}) {
+	c.List = append(c.List, value)
+}
+
+func (c *DictHolder) Add(value interface{}) {
+	if c.Key == "" {
+		c.Key = string(value.([]byte))
+	} else {
+		c.Dict[c.Key] = value
+		// reset
+		c.Key = ""
+	}
+}
+
+func (c *ValueHolder) Add(value interface{}) {
+	c.Val = value
+}
+
+// Obj methods
+
+func (c *ListHolder) Obj() interface{} {
+	return c.List
+}
+
+func (c *DictHolder) Obj() interface{} {
+	return c.Dict
+}
+
+func (c *ValueHolder) Obj() interface{} {
+	return c.Val
+}
+
+// Empty methods
+
+func (c *ListHolder) Empty() bool {
+	return c.List == nil
+}
+
+func (c *DictHolder) Empty() bool {
+	return c.Dict == nil
+}
+
+func (c *ValueHolder) Empty() bool {
+	return c.Val == nil
+}
+
+// now let's do the actual parsing
+
+func parse2(container Holder, reader *bufio.Reader) Holder {
+	b, err := reader.ReadByte()
+	if err != nil {
+		return container
+	}
+	switch b {
+	case 'e':
+		return container
+	case 'i':
+		buff, err := reader.ReadBytes('e')
+		check(err)
+		val, err := strconv.Atoi(string(buff[:len(buff)-1]))
+		check(err)
+		container.Add(val)
+		return parse2(container, reader)
+	case 'l':
+		c := parse2(&ListHolder{List: make([]interface{}, 0)}, reader)
+		if !container.Empty() {
+			container.Add(c.(*ListHolder).List)
+		} else {
+			container = c
+		}
+		return parse2(container, reader)
+	case 'd':
+		c := parse2(&DictHolder{Dict: make(map[string]interface{})}, reader)
+		if !container.Empty() {
+			container.Add(c.(*DictHolder).Dict)
+		} else {
+			container = c
+		}
+		return parse2(container, reader)
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		buff, err := reader.ReadBytes(':')
+		check(err)
+		strLen := string(b)
+		if len(buff) > 1 {
+			strLen += string(buff[:len(buff)-1])
+		}
+		strLenInt, err := strconv.Atoi(strLen)
+		check(err)
+		val := make([]byte, 0)
+		for i := 0; i < strLenInt; i++ {
+			b, err = reader.ReadByte()
+			val = append(val, b)
+		}
+		container.Add(val)
+		return parse2(container, reader)
+	}
+	return container
+}
+
+func ParseBencoded2(r io.Reader) interface{} {
+	reader := bufio.NewReader(r)
+
+	// kick this off by passing an empty holder
+	container := parse2(&ValueHolder{}, reader)
+	return container.Obj()
+}
--- go-bt/src/data/bencode_test.go
+++ go-bt/src/data/bencode_test.go
@@ -3,11 +3,18 @@ package data_test
 import (
 	"bytes"
 	"go-bt/data"
+	"io"
 	"testing"
 )
 
 func TestBencodeParsing(t *testing.T) {
 
+	testCases := []struct {
+		f func(io.Reader) interface{}
+	}{
+		{data.ParseBencoded},
+		{data.ParseBencoded2},
+	}
 	// single integer
 	r := bytes.NewReader([]byte("i42e"))
 	ret := data.ParseBencoded(r)
@@ -53,6 +60,59 @@ func TestBencodeParsing(t *testing.T) {
 	// a map with a list
 	r = bytes.NewReader([]byte("d3:fooli42eee"))
 	retMap, _ = data.ParseBencoded(r).(map[string]interface{})
+	retSlice = retMap["foo"].([]interface{})
+	if len(retSlice) != 1 && retSlice[0] != 42 {
+		t.Errorf("expected {'foo': [42]}, got %v", ret)
+	}
+}
+
+func TestBencodeParsing2(t *testing.T) {
+
+	// single integer
+	r := bytes.NewReader([]byte("i42e"))
+	ret := data.ParseBencoded2(r)
+	if ret != 42 {
+		t.Errorf("expected 42, got %v", ret)
+	}
+
+	// string, below 10 chars
+	r = bytes.NewReader([]byte("3:foo"))
+	ret = string(data.ParseBencoded2(r).([]byte))
+	if ret != "foo" {
+		t.Errorf("expected 'foo', got %v", ret)
+	}
+
+	// string, above 10 chars
+	r = bytes.NewReader([]byte("12:foobarraboof"))
+	ret = string(data.ParseBencoded2(r).([]byte))
+	if ret != "foobarraboof" {
+		t.Errorf("expected 'foo', got %v", ret)
+	}
+
+	// list with one int
+	r = bytes.NewReader([]byte("li42ee"))
+	retSlice, _ := data.ParseBencoded2(r).([]interface{})
+	if len(retSlice) != 1 && retSlice[0] != 42 {
+		t.Errorf("expected [42], got %v", ret)
+	}
+
+	// list with two items
+	r = bytes.NewReader([]byte("li42ei43ee"))
+	retSlice, _ = data.ParseBencoded2(r).([]interface{})
+	if len(retSlice) != 2 && retSlice[0] != 42 && retSlice[1] != 43 {
+		t.Errorf("expected [42, 43], got %v", ret)
+	}
+
+	// a simple map
+	r = bytes.NewReader([]byte("d3:fooi42ee"))
+	retMap, _ := data.ParseBencoded2(r).(map[string]interface{})
+	if retMap["foo"] != 42 {
+		t.Errorf("expected [42], got %v", retMap)
+	}
+
+	// a map with a list
+	r = bytes.NewReader([]byte("d3:fooli42eee"))
+	retMap, _ = data.ParseBencoded2(r).(map[string]interface{})
 	retSlice = retMap["foo"].([]interface{})
 	if len(retSlice) != 1 && retSlice[0] != 42 {
 		t.Errorf("expected {'foo': [42]}, got %v", ret)
