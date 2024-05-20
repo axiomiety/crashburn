fb002f318460bacd6745a81da8e1f3920c586c7c
axiomiety <axiomiety@gmail.com>
2024-05-20 18:09:10 +0800

    [golang] playing with rate limits, fix-up

--- golang/ratelimitexample.go
+++ golang/ratelimitexample.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
 	"io/ioutil"
 	"log"
-    "encoding/json"
 	"net/http"
 	"sync"
 	"time"
-    "flag"
 )
 
 func main() {
-    var numWorkers = flag.Int("w", 20, "number of workers")
-    flag.Parse()
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	var numWorkers = flag.Int("w", 20, "number of workers")
+	var timeout = flag.Int("t", 5, "timeout, in seconds")
+	flag.Parse()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout)*time.Second)
 	defer cancel()
 
 	var wg sync.WaitGroup
 	for i := 0; i < *numWorkers; i++ {
 
 		go func(ctx context.Context, idx int) {
-		    wg.Add(1)
+			wg.Add(1)
 			defer wg.Done()
 			client := &http.Client{}
 			req, _ := http.NewRequest("GET", "https://api.exchange.coinbase.com/products/ETH-USD/book?level=2", nil)
@@ -35,11 +36,11 @@ func main() {
 					log.Printf("worker[%d] fetching URL\n", idx)
 					res, _ := client.Do(req)
 					body, _ := ioutil.ReadAll(res.Body)
-                    var data map[string]any
-                    _ = json.Unmarshal(body, &data)
-                    if val, ok := data["message"]; ok {
-                        log.Println(val)
-                    }
+					var data map[string]any
+					_ = json.Unmarshal(body, &data)
+					if val, ok := data["message"]; ok {
+						log.Println(val)
+					}
 					defer res.Body.Close()
 				}
 
