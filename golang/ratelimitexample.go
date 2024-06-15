cd7b861665d90c80ab66582c5d0bb9e17dc2b6c1
axiomiety <axiomiety@gmail.com>
2024-06-16 07:02:26 +0800

    [golang] rate limts - fixed

--- golang/ratelimitexample.go
+++ golang/ratelimitexample.go
@@ -16,14 +16,16 @@ func rateLogger(ctx context.Context, countChan <-chan struct{}) {
     ticker := time.NewTicker(time.Second)
     count := 0
     defer ticker.Stop()
+    intervalStart := time.Now()
     for {
         select {
         case <- ctx.Done():
             break
         case <- countChan:
             count += 1
-        case <- ticker.C:
-            log.Printf("requests/second: %d ", count);
+        case intervalEnd := <- ticker.C:
+            log.Printf("requests/second: %d, elapsed: %s ", count, intervalEnd.Sub(intervalStart))
+            intervalStart = intervalEnd
             count = 0
         }
     }
@@ -32,18 +34,19 @@ func rateLogger(ctx context.Context, countChan <-chan struct{}) {
 func main() {
 	var numWorkers = flag.Int("w", 20, "number of workers")
 	var timeout = flag.Int("t", 5, "timeout, in seconds")
+	var reqsPerSecond = flag.Int("r", 15, "number of requests per seconds")
 	flag.Parse()
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout)*time.Second)
 	defer cancel()
 
 	var wg sync.WaitGroup
-    limiter := rate.NewLimiter(10, 15)
+    limiter := rate.NewLimiter(rate.Limit(*reqsPerSecond),1)
     countChan := make(chan struct{}, 100)
     go rateLogger(ctx, countChan)
     for i := 0; i < *numWorkers; i++ {
 
+		wg.Add(1)
 		go func(ctx context.Context, idx int, countChan chan<- struct{}) {
-			wg.Add(1)
 			defer wg.Done()
 			client := &http.Client{}
 			req, _ := http.NewRequest("GET", "https://api.exchange.coinbase.com/products/ETH-USD/book?level=2", nil)
@@ -61,15 +64,15 @@ func main() {
 					}
 					//log.Printf("worker[%d] fetching URL\n", idx)
 					res, _ := client.Do(req)
-					body, _ := ioutil.ReadAll(res.Body)
-                    // we completed a request! let the counter know
                     countChan <- struct{}{}
+					body, _ := ioutil.ReadAll(res.Body)
+					defer res.Body.Close()
+                    // we completed a request! let the counter know
 					var data map[string]any
 					_ = json.Unmarshal(body, &data)
 					if val, ok := data["message"]; ok {
 						log.Println(val)
 					}
-					defer res.Body.Close()
 				}
 
 			}
