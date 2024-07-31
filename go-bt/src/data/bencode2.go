eee54caa88c7b088bc952ecf9b97699d448f54b6
axiomiety <axiomiety@gmail.com>
2024-07-31 19:59:08 +0800

    go-bt: fix bencode parsing logic, tests for Encode

--- go-bt/src/data/bencode2.go
+++ go-bt/src/data/bencode2.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"reflect"
 	"slices"
 	"strconv"
@@ -13,34 +14,33 @@ import (
 // this is the interface that various containers implement
 
 type Holder interface {
-	Add(value interface{})
-	Obj() interface{}
-	// Empty() bool
+	Add(value any)
+	Obj() any
 }
 
 type ListHolder struct {
-	List []interface{}
+	List []any
 }
 
 type DictHolder struct {
-	Dict map[string]interface{}
+	Dict map[string]any
 	// tracks the current key
 	Key string
 }
 
 type ValueHolder struct {
-	Val interface{}
+	Val any
 }
 
 // Add methods
 
-func (c *ListHolder) Add(value interface{}) {
+func (c *ListHolder) Add(value any) {
 	c.List = append(c.List, value)
 }
 
-func (c *DictHolder) Add(value interface{}) {
+func (c *DictHolder) Add(value any) {
 	if c.Key == "" {
-		c.Key = string(value.([]byte))
+		c.Key = value.(string)
 	} else {
 		c.Dict[c.Key] = value
 		// reset
@@ -48,40 +48,26 @@ func (c *DictHolder) Add(value interface{}) {
 	}
 }
 
-func (c *ValueHolder) Add(value interface{}) {
+func (c *ValueHolder) Add(value any) {
 	c.Val = value
 }
 
 // Obj methods
 
-func (c *ListHolder) Obj() interface{} {
+func (c *ListHolder) Obj() any {
 	return c.List
 }
 
-func (c *DictHolder) Obj() interface{} {
+func (c *DictHolder) Obj() any {
 	return c.Dict
 }
 
-func (c *ValueHolder) Obj() interface{} {
+func (c *ValueHolder) Obj() any {
 	return c.Val
 }
 
-// Empty methods
-/*
-func (c *ListHolder) Empty() bool {
-	return c.List == nil
-}
-
-func (c *DictHolder) Empty() bool {
-	return c.Dict == nil
-}
-
-func (c *ValueHolder) Empty() bool {
-	return c.Val == nil
-}
-*/
 // now let's do the actual parsing
-func parse2(container Holder, reader *bufio.Reader) Holder {
+func parseBencodeStream(container Holder, reader *bufio.Reader) Holder {
 	b, err := reader.ReadByte()
 	if err != nil {
 		return container
@@ -95,23 +81,15 @@ func parse2(container Holder, reader *bufio.Reader) Holder {
 		val, err := strconv.Atoi(string(buff[:len(buff)-1]))
 		check(err)
 		container.Add(val)
-		return parse2(container, reader)
+		return parseBencodeStream(container, reader)
 	case 'l':
-		c := parse2(&ListHolder{List: make([]interface{}, 0)}, reader)
-		// if !container.Empty() {
+		c := parseBencodeStream(&ListHolder{List: make([]any, 0)}, reader)
 		container.Add(c.(*ListHolder).List)
-		// } else {
-		// container = c
-		// }
-		return parse2(container, reader)
+		return parseBencodeStream(container, reader)
 	case 'd':
-		c := parse2(&DictHolder{Dict: make(map[string]interface{})}, reader)
-		// if !container.Empty() {
+		c := parseBencodeStream(&DictHolder{Dict: make(map[string]any)}, reader)
 		container.Add(c.(*DictHolder).Dict)
-		// } else {
-		// container = c
-		// }
-		return parse2(container, reader)
+		return parseBencodeStream(container, reader)
 	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
 		buff, err := reader.ReadBytes(':')
 		check(err)
@@ -121,27 +99,27 @@ func parse2(container Holder, reader *bufio.Reader) Holder {
 		}
 		strLenInt, err := strconv.Atoi(strLen)
 		check(err)
-		val := make([]byte, 0)
+		val := make([]byte, strLenInt)
 		for i := 0; i < strLenInt; i++ {
 			b, err = reader.ReadByte()
 			check(err)
-			val = append(val, b)
+			val[i] = b
 		}
-		container.Add(val)
-		return parse2(container, reader)
+		container.Add(string(val[:]))
+		return parseBencodeStream(container, reader)
 	}
 	return container
 }
 
-func ParseBencoded2(r io.Reader) interface{} {
+func ParseBencoded2(r io.Reader) any {
 	reader := bufio.NewReader(r)
 
 	// kick this off by passing an empty holder
-	container := parse2(&ValueHolder{}, reader)
+	container := parseBencodeStream(&ValueHolder{}, reader)
 	return container.Obj()
 }
 
-func fillStruct(o interface{}, d map[string]interface{}) {
+func fillStruct(o any, d map[string]any) {
 	var structure reflect.Type
 	if reflect.TypeOf(o).Kind() != reflect.Struct {
 		structure = reflect.TypeOf(o).Elem()
@@ -157,7 +135,7 @@ func fillStruct(o interface{}, d map[string]interface{}) {
 				reflect.ValueOf(o).Elem().Field(i).Set(bindat)
 			} else {
 				oo := reflect.New(f.Type)
-				fillStruct(oo.Interface(), val.(map[string]interface{}))
+				fillStruct(oo.Interface(), val.(map[string]any))
 				reflect.ValueOf(o).Elem().Field(i).Set(oo.Elem())
 			}
 		}
@@ -166,7 +144,7 @@ func fillStruct(o interface{}, d map[string]interface{}) {
 
 func ParseTorrentFile2(r io.Reader) *BETorrent {
 	obj := ParseBencoded2(r)
-	d, ok := obj.(map[string]interface{})
+	d, ok := obj.(map[string]any)
 	if !ok {
 		panic("Unable to parse torrent")
 	}
@@ -175,14 +153,15 @@ func ParseTorrentFile2(r io.Reader) *BETorrent {
 	return betorrent
 }
 
-func Encode(buffer *bytes.Buffer, o interface{}) {
+func Encode(buffer *bytes.Buffer, o any) {
 	value := reflect.ValueOf(o)
+	log.Printf("found %s for %s", value.Kind(), o)
 	switch value.Kind() {
 	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64:
 		buffer.WriteByte('i')
 		buffer.WriteString(strconv.Itoa(int(value.Int())))
 		buffer.WriteByte('e')
-	case reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		buffer.WriteByte('i')
 		buffer.WriteString(strconv.Itoa(int(value.Uint())))
 		buffer.WriteByte('e')
@@ -192,9 +171,9 @@ func Encode(buffer *bytes.Buffer, o interface{}) {
 		buffer.WriteString(value.Interface().(string))
 	case reflect.Slice:
 		buffer.WriteByte('l')
-		// so this is a bit funky - we can't convert e.g. []int to []interface{}
+		// so this is a bit funky - we can't convert e.g. []int to []any
 		// directly
-		temp := make([]interface{}, value.Len())
+		temp := make([]any, value.Len())
 
 		for i := 0; i < value.Len(); i++ {
 			temp[i] = value.Index(i).Interface()
@@ -205,9 +184,9 @@ func Encode(buffer *bytes.Buffer, o interface{}) {
 		buffer.WriteByte('e')
 	case reflect.Map:
 		buffer.WriteByte('d')
-		temp := make(map[string]interface{}, value.Len())
+		temp := make(map[string]any, value.Len())
 
-		// we need a map[string]interface{}
+		// we need a map[string]any
 		iter := value.MapRange()
 		for iter.Next() {
 			k := iter.Key()
@@ -232,5 +211,4 @@ func Encode(buffer *bytes.Buffer, o interface{}) {
 	default:
 		panic(fmt.Sprintf("can't handle type %s", value.Kind()))
 	}
-
 }
--- go-bt/src/data/bencode_test.go
+++ go-bt/src/data/bencode_test.go
@@ -8,12 +8,90 @@ import (
 	"testing"
 )
 
+func TestBencodeDecode(t *testing.T) {
+
+	testCases := []struct {
+		data []byte
+	}{
+		// {[]byte("i-42e")},
+		{[]byte("3:foo")},
+		// {[]byte("12:foobarraboof")},
+		// {[]byte("li42ee")},
+		// {[]byte("li42ei43ee")},
+		// {[]byte("d3:fooi42ee")},
+		// {[]byte("d3:fooli42eee")},
+	}
+
+	buf := &bytes.Buffer{}
+	for _, testCase := range testCases {
+		buf.Reset()
+		data.Encode(buf, data.ParseBencoded2(bytes.NewReader(testCase.data)))
+		if bytes.Compare(buf.Bytes(), testCase.data) != 0 {
+			t.Errorf("expected %s, got %s", testCase.data, buf.Bytes())
+		}
+	}
+
+}
+func TestBencodeRecursiveParser(t *testing.T) {
+
+	// negative int!
+	r := bytes.NewReader([]byte("i-42e"))
+	ret := data.ParseBencoded2(r)
+	if ret != -42 {
+		t.Errorf("expected -42, got %v", ret)
+	}
+
+	// string, below 10 chars
+	r = bytes.NewReader([]byte("3:foo"))
+	ret = data.ParseBencoded2(r).(string)
+	if ret != "foo" {
+		t.Errorf("expected 'foo', got %v", ret)
+	}
+
+	// string, above 10 chars
+	r = bytes.NewReader([]byte("12:foobarraboof"))
+	ret = data.ParseBencoded2(r).(string)
+	if ret != "foobarraboof" {
+		t.Errorf("expected 'foo', got %v", ret)
+	}
+
+	// list with one int
+	r = bytes.NewReader([]byte("li42ee"))
+	retSlice, _ := data.ParseBencoded2(r).([]any)
+	if len(retSlice) != 1 && retSlice[0] != 42 {
+		t.Errorf("expected [42], got %v", ret)
+	}
+
+	// list with two items
+	r = bytes.NewReader([]byte("li42ei43ee"))
+	retSlice, _ = data.ParseBencoded2(r).([]any)
+	if len(retSlice) != 2 && retSlice[0] != 42 && retSlice[1] != 43 {
+		t.Errorf("expected [42, 43], got %v", ret)
+	}
+
+	// a simple map
+	r = bytes.NewReader([]byte("d3:foo3:bare"))
+	// r = bytes.NewReader([]byte("d3:fooi42ee"))
+	retMap, _ := data.ParseBencoded2(r).(map[string]any)
+	if retMap["foo"] != "bar" {
+		t.Errorf("expected {'foo': 'bar'}, got %v", retMap)
+	}
+
+	// a map with a list
+	r = bytes.NewReader([]byte("d3:fooli42eee"))
+	retMap, _ = data.ParseBencoded2(r).(map[string]any)
+	retSlice = retMap["foo"].([]interface{})
+	if len(retSlice) != 1 && retSlice[0] != 42 {
+		t.Errorf("expected {'foo': [42]}, got %v", ret)
+	}
+}
+
 func TestBencodeParsing(t *testing.T) {
 
 	testCases := []struct {
 		f func(io.Reader) interface{}
 	}{
-		{data.ParseBencoded},
+		// {data.ParseBencoded},
 		{data.ParseBencoded2},
 	}
 	for _, tc := range testCases {
@@ -26,14 +104,14 @@ func TestBencodeParsing(t *testing.T) {
 
 		// string, below 10 chars
 		r = bytes.NewReader([]byte("3:foo"))
-		ret = string(tc.f(r).([]byte))
+		ret = tc.f(r).(string)
 		if ret != "foo" {
 			t.Errorf("expected 'foo', got %v", ret)
 		}
 
 		// string, above 10 chars
 		r = bytes.NewReader([]byte("12:foobarraboof"))
-		ret = string(tc.f(r).([]byte))
+		ret = tc.f(r).(string)
 		if ret != "foobarraboof" {
 			t.Errorf("expected 'foo', got %v", ret)
 		}
